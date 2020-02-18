2e291e08d96f647be696a1cf78cd262062453dc4
Jeff <jmonnette@gmail.com>
2020-02-18 16:53:37 -0500

    add timestamp to output

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace docker_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello World! {DateTime.Now}");
         }
     }
 }
